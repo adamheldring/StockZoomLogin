e565530d1e51b97f72faaea241a12b4fcb5aec00
A-Game <javascript@adamheldring.com>
2018-11-17 11:48:32 +0100

    Added trend and single view details

--- src/components/singlePortfolio/singlePortfolio.js
+++ src/components/singlePortfolio/singlePortfolio.js
@@ -35,42 +35,55 @@ render() {
   const { portfolioPositions, availableCash, currency } = this.state
   return (
     <div className="myPageView">
-      <Link to="/"><h4 className="navBack">&#8592; GO BACK</h4></Link>
-      <h2>PORTFOLIO DETAILS</h2>
-      <h4>ID: {this.props.match.params.id}</h4>
-      <h4>Available Cash: { availableCash && `${parseInt(availableCash)} ${currency}`}</h4>
-      <h3>Positions:</h3>
+      <Link to="/"><h4 className="navBack">&#8592; BACK TO MY PAGE</h4></Link>
+      <h1>PORTFOLIO DETAILS</h1>
+      <p><strong>ID:</strong> {this.props.match.params.id}</p>
+      <p><strong>Available:</strong> { availableCash && `${parseInt(availableCash)} ${currency}`}</p>
+      <h2>POSITIONS:</h2>
       {portfolioPositions.length > 0 ?
         (<div className="portfolioList-container--single">
           {portfolioPositions.map((position, index) => (
             <div key={index} className="position-container">
-              <h4>{position.instrument.name}</h4>
-              <p>PRICE:</p>
-              <ul>
-                <li>
-                  Today: {position.instrument.price_today}{" "}
-                  {position.instrument.currency}
-                </li>
-                <li>
-                  Opening: {position.instrument.price_open}{" "}
-                  {position.instrument.currency}
-                </li>
-                <li>
-                  High: {position.instrument.price_high}{" "}
-                  {position.instrument.currency}
-                </li>
-                <li>
-                  Low: {position.instrument.price_low}{" "}
-                  {position.instrument.currency}
-                </li>
-                <li>
-                  Closing: {position.instrument.price_close}{" "}
-                  {position.instrument.currency}
-                </li>
-              </ul>
-              <p>Kind: {position.instrument.kind}</p>
-              <p>Rating: {position.instrument.rating}</p>
-
+              <h4 className="position-name">{position.instrument.name.toUpperCase()}</h4>
+              <p>
+                <strong>Kind:</strong> {position.instrument.kind}<br />
+                <strong>Rating:</strong> {position.instrument.rating}
+              </p>
+              <div className="value-section">
+                <table>
+                  <thead>
+                    <tr><th>PRICE:</th></tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Today:</td>
+                      <td>{position.instrument.price_today}{" "}{position.instrument.currency}</td>
+                    </tr>
+                    <tr>
+                      <td>Open:</td>
+                      <td>{position.instrument.price_open}{" "}{position.instrument.currency}</td>
+                    </tr>
+                    <tr>
+                      <td>High:</td>
+                      <td>{position.instrument.price_high}{" "}{position.instrument.currency}</td>
+                    </tr>
+                    <tr>
+                      <td>Low:</td>
+                      <td>{position.instrument.price_low}{" "}{position.instrument.currency}</td>
+                    </tr>
+                    <tr>
+                      <td>Close:</td>
+                      <td>{position.instrument.price_close}{" "}{position.instrument.currency}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div className="trendArrow-container">
+                  {(position.instrument.price_close >= position.instrument.price_open) ?
+                    <img className="trendArrow"src="../arrowup.png" alt="Up trend"/>
+                    : <img className="trendArrow" src="../arrowdown.png" alt="Down trend" />
+                  }
+                </div>
+              </div>
               <p>
                 {position.instrument.company &&
                   position.instrument.company.description}
